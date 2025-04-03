bfcd8a3adc3429825d6ad0b07ba2f802db9c2fc6
chyiyenwang <chyiyenwang@gmail.com>
2025-04-03 00:10:34 -0700

    added provider and updated global styles

---
+++ src/app/providers.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { HeroUIProvider } from '@heroui/react';
+
+interface ProviderProps {
+  children: React.ReactNode
+}
+
+export default function Providers({ children }: ProviderProps) {
+  return (
+    <HeroUIProvider>
+      {children}
+    </HeroUIProvider>
+  )
+}
